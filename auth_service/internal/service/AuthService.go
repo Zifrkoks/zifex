bdbff74d8b099191408e064af56aeec1c664396e
Zifrkoks <qweasdzxcwsxxsw1234@gmail.com>
2025-03-16 10:04:43 +0300

    authutil added

--- auth_service/internal/service/AuthService.go
+++ auth_service/internal/service/AuthService.go
@@ -8,3 +8,16 @@ import (
 type AuthService struct {
 	users *UserRepository
 }
+
+func (service AuthService) Reg(oldP string, newP string) error {
+	return nil
+}
+
+func (service AuthService) Login(login string, password string) (token string, err error) {
+
+	return "", nil
+}
+
+func (service AuthService) ChangePassword(oldP string, newP string) error {
+	return nil
+}

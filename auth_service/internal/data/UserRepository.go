9c16bb1487ce4f38b4b0cf14a51166d5302c2e65
Zifrkoks <qweasdzxcwsxxsw1234@gmail.com>
2025-03-19 22:54:48 +0300

    in develop

--- auth_service/internal/data/UserRepository.go
+++ auth_service/internal/data/UserRepository.go
@@ -7,6 +7,14 @@ import (
 type UserRepository struct {
 }
 
+func (r *UserRepository) Create(user *User) error {
+	panic("unimplemented")
+}
+
+func (r *UserRepository) Update(user *User) error {
+	panic("unimplemented")
+}
+
 func (r UserRepository) GetById(id uint) (*User, error) {
 	return nil, nil
 }
@@ -17,4 +25,15 @@ func (r UserRepository) GetByUsername(username string) (*User, error) {
 
 func (r UserRepository) AddPermisions(usernameOrId interface{}, permissions map[string]string) (bool, error) {
 	return false, nil
+}
+
+/*
+you need to get error from errChannel for make sure that work with user is closed
+*/
+func (r UserRepository) GetByUsernamePromise(username string) (user *User, errChannel chan error) {
+	return nil, make(chan error)
+}
+
+func (r UserRepository) CreateUserRefreshTokenPromise(username string) (refresh *RefreshToken, errChannel chan error) {
+	return nil, make(chan error)
 }
--- auth_service/internal/service/AuthService.go
+++ auth_service/internal/service/AuthService.go
@@ -14,28 +14,38 @@ type AuthService struct {
 	users *UserRepository
 }
 
-func (service AuthService) Reg(login string, password string) error {
+func (service AuthService) Reg(username string, password string) (err error) {
+
+	user, errch := service.users.GetByUsernamePromise(username)
+	bytes := sha256.Sum256([]byte(password))
+	password = string(bytes[:])
+	err = <-errch
+	if user != nil {
+		return errors.New("username unavailable")
+	}
+	user = &User{Username: username, Password: password}
+	err = service.users.Create(user)
+	if err != nil {
+		return errors.New("db error")
+	}
 	return nil
 }
 
-func (service AuthService) Login(login string, password string) (token string, err error) {
-	var passCh chan string
-	var user *User
-	go func() {
-		user, err = service.users.GetByUsername(login)
-		passCh <- user.Password
-	}()
-	hash := sha256.New()
-	hash.Write([]byte(password))
-	password = string(hash.Sum(nil))
-	userPass := <-passCh
+func (service AuthService) Login(username string, password string) (token string, err error) {
+
+	user, errch := service.users.GetByUsernamePromise(username)
+
+	bytes := sha256.Sum256([]byte(password))
+	password = string(bytes[:])
+	err = <-errch
 	if err != nil {
 		return "", errors.New("user not found")
 	}
 
-	if password != userPass {
+	if password != user.Password {
 		return "", errors.New("password wrong")
 	}
+
 	builder := NewTokenBuilder()
 	builder.AddToHeader("type", "jwt")
 	builder.AddToHeader("alg", "HS256")
@@ -49,14 +59,32 @@ func (service AuthService) Login(login string, password string) (token string, e
 	return
 }
 
-func (service AuthService) ChangePassword(newP string) error {
+func (service AuthService) ChangePassword(newP string, username string) (err error) {
+	user, errch := service.users.GetByUsernamePromise(username)
+	bytes := sha256.Sum256([]byte(newP))
+	err = <-errch
+	if err != nil {
+		return errors.New("user repo error:" + err.Error())
+	}
+	user.Password = string(bytes[:])
+	err = service.users.Update(user)
+	if err != nil {
+		return errors.New("db error:" + err.Error())
+	}
 	return nil
 }
 
 func (service AuthService) ValidateToken(token string) error {
+	return ValidateToken(token, viper.GetString("jwt.secret"))
+}
+
+func (service AuthService) RefreshToken(oldRT string) (jwt string, newRT string, err error) {
+	return
+}
+func (service AuthService) AddPermisions(map[string]string) error {
 	return nil
 }
 
-func (service AuthService) AddPermisions(map[string]string) error {
+func (service AuthService) CheckUsernameIsAvailble(username string) error {
 	return nil
 }
